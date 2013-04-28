51965ea17dedeb8c2787aa264605b726cbce4f35
Nilesh <nilesh@londhe.com>
2013-04-28 14:14:07 -0700

    initial commit

---
+++ jq.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+
+    Meteor.startup(function () {
+      $('#myDiv').append("foo");
+      console.log('bar');
+    });
+
+  Template.hello.greeting = function () {
+    return "Welcome to jq.";
+  };
+
+  Template.hello.events({
+    'click input' : function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
